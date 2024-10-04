d2b31d5bfcd4ac123aac82fcf32f5d639b7cd493
Michaelangelo Ojeani <G00416140@atu.ie>
2024-10-04 13:43:35 +0000

    Created the header and footer files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Content from './components/content';
+import Footer from './components/Footer';
 
 function App() {
   let x = 5;
