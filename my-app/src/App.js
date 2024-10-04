af9cd89088b5842a753425dc815cc2481b4ed277
Michaelangelo Ojeani <G00416140@atu.ie>
2024-10-04 13:58:38 +0000

    exercise 3 complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,8 +8,9 @@ function App() {
   let x = 5;
   return (
     <div className="App">
-     <h1>Hello World</h1>
-     <h2>It is {new Date().toLocaleTimeString()}.</h2>
+   <Header></Header>
+    <Content/>
+    <Footer></Footer>
     </div>
   );
 }
