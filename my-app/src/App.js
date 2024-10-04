74e9d823447766d2752d7c0e964fdae0f7c3e6fe
Michaelangelo Ojeani <G00416140@atu.ie>
2024-10-04 14:08:55 +0000

    exercise 4 complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/navbar';
 import Header from './components/Header';
-import Content from './components/content';
 import Footer from './components/Footer';
+import Content from './components/content';
 
 function App() {
-  let x = 5;
   return (
-    <div className="App">
-   <Header></Header>
-    <Content/>
-    <Footer></Footer>
-    </div>
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<h1>Read Component</h1>} />
+        <Route path="/create" element={<h1>Create Component</h1>} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
