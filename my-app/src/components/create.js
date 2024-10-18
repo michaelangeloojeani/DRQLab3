effe7016773cf4fcf8fd4e1a8ab585288117aca6
Michaelangelo Ojeani <G00416140@atu.ie>
2024-10-18 14:09:51 +0000

    Create component modified

--- my-app/src/components/create.js
+++ my-app/src/components/create.js
@@ -1,23 +1,31 @@
-import {useState} from "react";
 
-const Create = () => {
 
-  const[title, setTitle]
+import { useState } from "react";
 
-    return <div>
-    <h3>Hello form create component</h3>
-    <form onSubmit={handleSubmit}>
-    <div className="form-group">
-  <label>Add Movie Title: </label>
-  <input type="text"
-    className="form-control"
-    value={title}
-    onChange={(e) => { setTitle(e.target.value) }} 
-  />
-</div>
-      </form>
-
-    </div>
-  };
+function Create() {
+  const [title, setTitle] = useState('');
   
-  export default Create;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title);
+  }
+
+  return (
+    <div>
+      <h2>This is my Create Component.</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Add Movie Title: </label>
+          <input type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
+        </div>
+        <input type="submit" value="Add Movie" />
+      </form>
+    </div>
+  );
+}
+
+export default Create;
